fd1598aa7546f0ce70308d98db2ff268b0afa498
Mushus <mushus.wynd@gmail.com>
2015-10-08 14:59:33 +0900

    fix any bugs, code format and readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Singlejson parser plugin for Embulk
 
-TODO: Write short description here and embulk-parser-singlejson.gemspec file.
+This is convinience parser for Embulk.
 
 ## Overview
 
@@ -9,23 +9,74 @@ TODO: Write short description here and embulk-parser-singlejson.gemspec file.
 
 ## Configuration
 
-- **shema**: description (array, required)
+- **foreach**: ruby expression to make multiple record (string, default: nil)
+- **exclude**: ruby expression to exclude record (string, default: "false")
+- **shema**: record shema (array, default: nil)
 
 ## Example
 
+config.yml:
 ```yaml
 in:
-  type: any file input plugin type
+  type: file
+  path_prefix: C:\develop\project\diw\embulk\sample.json
   parser:
-    type: singlejson
-    shema: example1
+    type: convjson
+    foreach: json
+    exclude: "key == 'junk_data'"
+    schema:
+      - {name: key, type: string, exp: "key"}
+      - {name: id, type: long, exp: "index"}
+      - {name: message, type: string, exp: "value['message']"}
+      - {name: add, type: double, exp: "value['int'] + value['float']"}
+      - {name: now, type: string, exp: "Time.now"}
+      - {name: time, type : timestamp, exp: "value['date']", format: "%Y/%m/%d"}
+```
+
+sample.json:
+```json
+{
+    "junk_data": {
+        "message": "I'm not need."
+    },
+    "a":{
+        "message": "hello world!",
+        "int": 1,
+        "float": 0.1,
+        "child": {
+            "message": "I'm child."
+        },
+        "date": "2015/01/01"
+    },
+    "b":{
+        "message": "hello world!",
+        "int": 1,
+        "float": 0.1,
+        "child": {
+            "message": "I'm child."
+        },
+        "date": "2015/10/10"
+    }
+}
 ```
 
 (If guess supported) you don't have to write `parser:` section in the configuration file. After writing `in:` section, you can let embulk guess `parser:` section using this command:
 
+TODO: registing gem dir
+
 ```
 $ embulk gem install embulk-parser-singlejson
 $ embulk guess -g singlejson config.yml -o guessed.yml
+```
+
+result:
+```
++------------+---------+----------------+------------+---------------------------+-------------------------+
+| key:string | id:long | message:string | add:double |                now:string |          time:timestamp |
++------------+---------+----------------+------------+---------------------------+-------------------------+
+|          a |       1 |   hello world! |        1.1 | 2015-10-08 14:57:06 +0900 | 2014-12-31 15:00:00 UTC |
+|          b |       2 |   hello world! |        1.1 | 2015-10-08 14:57:06 +0900 | 2015-10-09 15:00:00 UTC |
++------------+---------+----------------+------------+---------------------------+-------------------------+
 ```
 
 ## Build
--- lib/embulk/parser/convjson.rb
+++ lib/embulk/parser/convjson.rb
@@ -1,4 +1,4 @@
-require 'json'
+require "json"
 
 module Embulk
 	module Parser
@@ -8,48 +8,57 @@ module Embulk
 			def self.transaction(config, &control)
 				# 設定の読み込み
 				task = {
-                    :foreach => config.param('foreach', :string),
-					:schema => config.param('schema', :array)
+                    :foreach => config.param("foreach", :string, default: 1),
+					:exclude => config.param("exclude", :string, default: "false"),
+					:schema => config.param("schema", :array, default: nil)
 				}
 
                 # レコードのカラム(詳細は schema 定義に従う)
                 columns = task[:schema].each_with_index.map do |column, index|
                     Column.new(index, column["name"], column["type"].to_sym)
                 end
-p "here"
 				yield(task, columns)
+			end
+
+			def init
+				@foreach = task["foreach"]
+				@exclude = task["exclude"]
+				@schema = task["schema"]
 			end
 
 			def run(file_input)
 				# ファイル毎に1レコード
                 while file = file_input.next_file
                     json = JSON.load(file.read)
-                    foreach = evaluate_foreach(json, @task["foreach"])
+                    foreach = evaluate_foreach(json, @foreach)
                     case foreach.class.to_s
                     when "Array"
                         foreach.each_with_index.map do |column, index|
-                            @page_builder.add(make_record(json, index, column, index))
+							next if evaluate_exp(json, index, column, index, @exclude)
+	                        @page_builder.add(make_record(json, index, column, index))
                         end
                     when "Hash"
                         foreach.each_with_index do |(key, value), index|
-                            @page_builder.add(make_record(json, index, value, index))
+							next if evaluate_exp(json, key, value, index, @exclude)
+                            @page_builder.add(make_record(json, key, value, index))
                         end
                     else
-                        p "here"
+						return if evaluate_exp(json, index, column, index, @exclude)
                         @page_builder.add(make_record(json, nil, nil, nil))
                     end
                 end
 				page_builder.finish
 			end
 
+			private
+
             # レコードを作成
             def make_record(json, key, value, index)
-                schema = @task["schema"]
-                schema.each_with_index.map do |column|
-                    name = column['name'];
-                    exp = column['exp'];
-                    type = column['type'];
-					format = person['format']? ? column['format']: nil;
+                @schema.each_with_index.map do |column|
+                    name = column["name"]
+                    exp = column["exp"]
+                    type = column["type"]
+					format = column["format"]
                     convert_type(evaluate_exp(json, key, value, index, exp), type, format)
                 end
             end
@@ -72,20 +81,24 @@ p "here"
 
             # valをtype型に変換する
             def convert_type(val, type, format)
-                case type
-                when "string"
-                    val
-                when "long"
-                    val.to_i
-                when "double"
-                    val.to_f
-                when "boolean"
-                    ["yes", "true", "1"].include?(val.downcase)
-                when "timestamp"
-                    val.empty? ? nil : Time.strptime(val, format)
-                else
-                    raise "Unsupported type #{type}"
-                end
+				if val.class.to_s == type then
+					val
+				else
+                	case type
+                	when "string"
+                    	val.to_s
+                	when "long"
+                    	val.to_i
+                	when "double"
+                    	val.to_f
+                	when "boolean"
+                    	["yes", "true", "1"].include?(val.downcase)
+                	when "timestamp"
+                    	val.empty? ? nil : Time.strptime(val, format)
+                	else
+                    	raise "Unsupported type #{type}"
+                	end
+				end
             end
 
 		end
